62835a9a7c5ce3f6557e53de412e5682d719667f
Jelo Dimaano <jannangelodimaano@gmail.com>
2022-12-05 19:14:58 +0800

    Change asterisk into App and define geometry

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,12 @@
 import customtkinter
 from PIL import Image
-from utils.main_menu import *
+from utils.main_menu import App
 
 
 class WelcomeScreen():
     def __init__(self, ctk):
         self.frame = customtkinter.CTkFrame(master=ctk)
+        ctk.geometry('600x400')
         self.frame.pack(pady=20, padx=60, fill='both', expand=True)
 
     def welcome_screen(self):
@@ -16,7 +17,7 @@ class WelcomeScreen():
         label_image = customtkinter.CTkLabel(master=self.frame, text="", image=image_icon_image)
         label_image.pack(pady=(20, 0), padx=10)
         # Welcome Screen
-        welcome_label = customtkinter.CTkLabel(master=self.frame, text="Welcome to PCFIND", font=('Montserrat', 14),
+        welcome_label = customtkinter.CTkLabel(master=self.frame, text="Welcome to PCFIND", font=('Montserrat', 14, 'bold'),
                                                text_color='#2FA572')
         welcome_label.pack(pady=(20, 0), padx=10)
         description_label = customtkinter.CTkLabel(master=self.frame,
